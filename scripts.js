1ea44e50995977b2a589f3ad3763a459b8e2da7b
ctmcquilkin <ctmcquilkin@gmail.com>
2024-09-27 22:18:02 -0500

    feat add chooseOperation function

--- scripts.js
+++ scripts.js
@@ -11,44 +11,58 @@ class Calculator {
         this.previousOperandTextElement = previousOperandTextElement;
         this.currentOperandTextElement = currentOperandTextElement;
         this.clear();
-    }
+    };
 
     clear() {
-        this.currentOperand = ''
-        this.previousOperand = ''
-        this.operation = undefined
-    }
+        this.currentOperand = '';
+        this.previousOperand = '';
+        this.operation = undefined;
+    };
 
     delete() {
 
-    }
+    };
 
     appendNumber(number) {
         if (number === '.' && this.currentOperand.includes('.')) return;
         this.currentOperand = this.currentOperand.toString() + number.toString();
 
-    }
+    };
 
     chooseOperation(operation) {
-
-    }
+        if (this.currentOperand === '') return;
+        if (this.previousOperand !== '') {
+            this.compute();
+        };
+        this.operation = operation;
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = '';
+    };
 
     compute() {
 
-    }
+    };
 
     updateDisplay() {
         this.currentOperandTextElement.innerText = this.currentOperand;
+        this.previousOperandTextElement.innerText = this.previousOperand;
 
-    }
+    };
 
-}
+};
 
 const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
 
 numberButtons.forEach(button => {
     button.addEventListener('click', () => {
         calculator.appendNumber(button.innerText);
+        calculator.updateDisplay();
+    })
+});
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText);
         calculator.updateDisplay();
     })
 });
